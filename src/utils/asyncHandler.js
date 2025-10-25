8c7c19eda86f1e7f85a45cba6f49467d2db73a6d
Rohit Sharma <rohitrs1209@gmail.com>
2025-10-25 20:19:36 +0530

    add register controller

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,8 @@
-const asyncHandlier=(requestHandler)=>{
-    (req,res,next)=>{
+const asyncHandler=(requestHandler)=>{
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
     }
 }
 
 
-export {asyncHandlier}
+export {asyncHandler}
